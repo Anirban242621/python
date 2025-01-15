095f06bb6ec298ef5ff05fda43565c724499c437
Anirban <itsboyraj@gmail.com>
2025-01-16 01:00:40 +0530

    Little Change

--- Interview/addListValue.py
+++ Interview/addListValue.py
@@ -4,8 +4,13 @@ def equalToTarget(arr,target):
         a=(target-arr[i])
         l1.append(a)
     common = list(set(l1) & set(arr))
+    if len(common)==0:
+        return False
     return common
-arr=[3,2,4,7,8]
+arr=[30,2,4,7,8]
 target=9
 a=equalToTarget(arr,target)
-print(a)
+if a!=False:
+    print("Found such pair which can match the Target: ",a)
+else:
+    print("No such pair found")
